f7b53ab0217fb16dcde554bc5690a9cc13d8ba8d
Kayolm <mkarasea4@gmail.com>
2021-12-08 20:22:49 +0300

    slm

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let passwordEl = document.getElementById('password');
 let chars = '0123456789abcdefghijklmnopqrstuvwxyz!£$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 let passwordLength = Number.MAX_SAFE_INTEGER / 200000000;
 let password = '';
+
 for(let i = 0; i < passwordLength; i++) {
     passwordEl.innerText += chars[Math.round(Math.random() * chars.length)];
-
 }
 // passwordEl.innerText = password;
